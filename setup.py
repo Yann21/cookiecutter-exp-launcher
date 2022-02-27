b11b098f2db0179ad6f30e317ef0c5a668ed7eaa
Yann Hoffmann <y.hoffmann@talkwalker.com>
2022-02-27 16:59:28 +0100

    Small changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
